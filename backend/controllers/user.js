56e5f70cb2b74d037bd5342ee5de6f4a3649b2fc
Lydia <lydiarugby@gmail.com>
2023-01-20 16:56:38 +0100

    install passwordValidator

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,33 +1,49 @@
 require('dotenv').config();
 
-// importer bcrypt
 const bcrypt = require('bcrypt');
-//importer jsonwebtoken
 const jwt = require('jsonwebtoken');
-
+const passwordValidator = require('password-validator');
 // importer model user
 const User = require('../models/User');
+
+// créer password plus fort
+const passwordSchema = new passwordValidator();
+
+passwordSchema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits()                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
 
 
 // inscrire user
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    // enregistrer hash du password,  enregistrer nouveau user
-    .then(hash => {        
-        // création nouvel user avec modèle mongoose
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error, message: 'Erreur création utilisateur' }));
+    // SI password non valide
+    if (!passwordSchema.validate(req.body.password)) {
+          return res.status(400).json({ message: 'Votre password doit avoir au minimum 8 caratères dont 1 majuscule, 1 minuscule et 1 chiffre' }); 
+    } // SINON password valide
+    else if (passwordSchema.validate(req.body.password)) {
+        bcrypt.hash(req.body.password, 10)
+        // enregistrer hash du password pr nouveau user
+        .then(hash => {        
+            // créer nouvel user avec modèle mongoose
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+            .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error, message: 'Erreur création utilisateur' }));
+    };    
 };
 
 
-// connecter user sans jsonwebtoken
+// connecter user avec jsonwebtoken
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
     .then(user => {
